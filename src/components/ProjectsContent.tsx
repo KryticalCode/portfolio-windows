3ab0f68e87c6e2a78278e2da8fe270ac08a8b867
KryticalCode <mattcirona@gmail.com>
2025-07-29 19:24:37 +1000

    Refactor: Enhance layout and styling of ProjectsContent component for improved presentation

--- src/components/ProjectsContent.tsx
+++ src/components/ProjectsContent.tsx
@@ -1,20 +1,20 @@
 export default function ProjectsContent() {
   return (
-    <div className="flex flex-col space-y-8">
+    <div className="flex flex-col space-y-8 ">
       {/* ------------------------------- */}
       {/* school projects section */}
       {/* ------------------------------- */}
       <section>
-        <h2 className="">school projects</h2>
+        <h2 className="mb-2">school projects</h2>
         <p className="font-light">
           These are some of the projects that I had worked on during my time at
           RMIT. Some were individual, while others were partner / group
           projects.
         </p>
-        <div className="flex flex-row flex-wrap gap-4 mt-4 bg-gray-100 p-4 rounded-lg shadow-md">
+        <div className="flex flex-row flex-wrap gap-12 mt-4 bg-gray-100 p-4 rounded-lg shadow-md">
           <a
             href="https://github.com/rmit-computing-technologies/Hologame"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -23,22 +23,22 @@ export default function ProjectsContent() {
               alt="Nintendogs-Like Unity Game"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">
+            <span className="text-center break-words mt-2">
               Hologame - Looking Glass Display
             </span>
           </a>
           <a
             href="https://github.com/rmit-fsd-2024-s1/s3625089-s3948914-a2"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              src=""
+              src="folder-empty.png"
               alt="Full Stack Node.js App"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">
+            <span className="text-center break-words mt-2">
               Full Stack Node.js App - RMIT Assignment
             </span>
           </a>
@@ -49,15 +49,15 @@ export default function ProjectsContent() {
       {/* personal projects section */}
       {/* ------------------------------- */}
       <section>
-        <h2>personal projects</h2>
+        <h2 className="mb-2">personal projects</h2>
         <p className="font-light">
           Independant projects that I have created, some for fun, some to
           reinforce key-areas in my learning
         </p>
-        <div className="flex flex-row flex-wrap gap-4 mt-4 bg-gray-100 p-4 rounded-lg shadow-md">
+        <div className="flex flex-row flex-wrap gap-12 mt-4 bg-gray-100 p-4 rounded-lg shadow-md">
           <a
             href="https://www.github.com/KryticalCode/"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -66,11 +66,11 @@ export default function ProjectsContent() {
               alt="to-do list app"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">To-Do List App</span>
+            <span className="text-center break-words mt-2">To-Do List App</span>
           </a>
           <a
             href="https://www.github.com/KryticalCode/portfolio-website"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -79,11 +79,13 @@ export default function ProjectsContent() {
               alt="Portfolio Website"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">(This) Portfolio Website</span>
+            <span className="text-center break-words mt-2">
+              (This) Portfolio Website
+            </span>
           </a>
           <a
             href="https://github.com/KryticalCode"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -92,13 +94,13 @@ export default function ProjectsContent() {
               alt="Full Stack C# Library Management System"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">
-              Full Stack C# Library Management System
+            <span className="text-center break-words mt-2">
+              Full Stack C# Simple Library Management System
             </span>
           </a>
           <a
             href="https://github.com/KryticalCode"
-            className=""
+            className="flex flex-col items-center max-w-80"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -107,7 +109,7 @@ export default function ProjectsContent() {
               alt="TO ADD"
               className="w-80 h-50 object-cover rounded-lg shadow-md"
             />
-            <span className="text-center">PROJECT TO ADD</span>
+            <span className="text-center break-words mt-2">PROJECT TO ADD</span>
           </a>
         </div>
       </section>
